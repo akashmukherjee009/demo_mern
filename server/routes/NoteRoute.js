a8474e024d5963b4ec0434a87bd043f002f4700a
Akash Mukherjee <mukherjeeakash009@gmail.com>
2024-01-05 15:50:42 +0530

    Mocha test Backend validation(Not completed)

--- server/routes/NoteRoute.js
+++ server/routes/NoteRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createNote, login } from '../controller/NoteController.js';
 import { verifyToken } from '../middleware/VarifyToken.js';
+import { body, validationResult } from 'express-validator'
 
 
 const router = express.Router();
@@ -9,7 +10,12 @@ router.get('', (req, res) => {
   res.json({ message: 'GET request received' });
 });
 
-router.post('', createNote);
+router.post('',[
+  body('name').notEmpty().withMessage('Username must be at least 5 characters long'),
+  body('email').notEmpty().isEmail().withMessage('Invalid email address'),
+  body('password').notEmpty().isLength({ min: 4 }).withMessage('Invalid email address'),
+  // Add more validation rules as needed
+], createNote);
  // const dataFromBody = req.body; 
   // console.log(dataFromBody);
   // res.json({ message: 'POST request received', data: dataFromBody });
